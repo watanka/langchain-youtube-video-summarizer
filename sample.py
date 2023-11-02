c550c3c0fce6f1e16b94cc99a28ce965b393b92f
eunsungshin <eunsung.shin@gmail.com>
2023-11-02 14:52:01 +0900

    StreamLit을 사용한 간단한 UI 작성

---
+++ sample.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+st.title('Langchain Youtube Summarizer:sunglasses:')
+
+
+url = st.text_input("요약이 필요한 Youtube URL을 입력해주세요.")
+
+if url:
+    # response = requests.post("http://localhost:8080/summary", json={"url": url})
+    # video_id = response.json()["video_id"]
+
+    
+    st.video(url)
+    
+    # st.error("Invalid YouTube URL")
