2e3f2c460684ae7977628865b4e3aadcb7e29eb2
eunsungshin <eunsung.shin@gmail.com>
2023-11-06 22:47:02 +0900

    SRP에 따라 pytube mp3 변환과 transcribe 부분 분리

--- transcriber/transcription.py
+++ transcriber/transcription.py
@@ -12,23 +12,11 @@ class YoutubeTranscriber(AbstractTransriber) :
     def __init__(self, transcribe_model) :
         self.transcribe_model = transcribe_model
 
-    def __call__(self, video_url : str) -> str :
-
-        # convert url to audio
-        yt = YouTube(video_url)
-        video_id = video_url.split('/')[-1].replace('watch?v=', '')
-        audio_path = os.getenv("AUDIO_PATH", 'audios')
-
-        # TODO match uuid with yt.title
-        audio_outputpath = str(os.path.join(audio_path, f'{str(uuid4())[:4]}_{video_id}.mp3'))
-        
-        yt.streams.filter(only_audio=True).first().download(filename = audio_outputpath)
-
-
-        result = self.transcribe_model.transcribe(audio_outputpath)
+    def __call__(self, video_path : str) -> str :
+        result = self.transcribe_model.transcribe(video_path)
 
         # get transcription_filename
-        transcript_filename = os.path.basename(os.path.splitext(audio_outputpath)[0] + '.txt')
+        transcript_filename = os.path.basename(os.path.splitext(video_path)[0] + '.txt')
         trscript_dir = os.getenv("TRANSCRIPTION_PATH", "transcriptions")
 
         trscript_path = os.path.join(trscript_dir, transcript_filename)
