0749d4c48266ceda4185eae07bf513e424df1e21
eunsungshin <eunsung.shin@gmail.com>
2023-11-07 17:16:41 +0900

    whisper 정보 저장을 위해 숨긴 내용 엔드포인트로 빼냄

--- transcriber/transcription.py
+++ transcriber/transcription.py
@@ -15,14 +15,4 @@ class YoutubeTranscriber(AbstractTransriber) :
     def __call__(self, video_path : str) -> str :
         result = self.transcribe_model.transcribe(video_path)
 
-        # get transcription_filename
-        transcript_filename = os.path.basename(os.path.splitext(video_path)[0] + '.txt')
-        trscript_dir = os.getenv("TRANSCRIPTION_PATH", "transcriptions")
-
-        trscript_path = os.path.join(trscript_dir, transcript_filename)
-
-        # write to the persistant space
-        with open(trscript_path, 'w') as f :
-            f.write(result['text'])
-
-        return trscript_path
+        return result['text']
