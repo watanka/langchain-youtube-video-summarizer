87e5dd61f997d9ea5bdb8e2d294f4e02b38fee78
eunsung <eunsung.shin@gmail.com>
2023-11-08 13:13:15 +0900

    ENVIRONMENT(test, production) 환경변수에 따라, local llm 도커 이미지 로드 여부 선택

---
+++ run-docker-compose.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ENVIRONMENT=${1:-production}
+
+echo "# Generate .dockerignore file" > .dockerignore
+
+if [ "$ENVIRONMENT" = "production" ]; then
+    echo "ENVIRONMENT SET AS PRODUCTION"
+    cat <<EOF >> .dockerignore
+# 여기에 production 환경에서 무시할 파일이나 디렉토리를 추가
+whisper/
+EOF
+
+elif [ "$ENVIRONMENT" = "test" ]; then
+    echo "ENVIRONMENT SET AS TEST"
+    cat <<EOF >> .dockerignore
+# 여기에 테스트 환경에서 무시할 파일이나 디렉토리를 추가
+whisper/
+summarizer/local_llm
+EOF
+
+else
+    echo "Unknown environment: $ENVIRONMENT"
+    exit 1
+fi
+
+# docker-compose build
+echo "RUNNING docker-compose up"
+ENVIRONMENT="$ENVIRONMENT" docker-compose up --build
